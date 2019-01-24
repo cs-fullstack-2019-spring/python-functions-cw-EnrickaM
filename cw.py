d209151433ba1fad4126118ad07517c50f26489f
EnrickaM <marshallenricka@yahoo.com>
2019-01-24 17:03:20 -0600

    message

--- cw.py
+++ cw.py
@@ -1,3 +1,6 @@
+# python functions classwork
+
+
 def main():
     #problem1()
     #problem2()
@@ -15,6 +18,10 @@ def problem1():
 def mynumbersFunction(number):
         return number
 
+## Problem 2:
+##### We will keep having this problem until EVERYONE gets it right without help
+#Create a function that has a loop that quits with ‘q’.
+# If the user doesn't enter 'q', ask them to input another string.
 
 
 def problem2():
@@ -24,6 +31,9 @@ def problem2():
 
 
 
+### Problem 3:
+#Create 4 functions called add, subtract, multiply, and divide.
+# Create them to allow a user to perform the name of the function to the two numbers and return the result.
 
 
 def problem3():
@@ -47,6 +57,10 @@ def multiply(e,m):
 
 def divide(o,w):
     return (o/w)
+    ### Problem 4:
+#Create a function that will ask the user for a number.
+# Use the function to get two numbers, then pass the two numbers to a function and ask a user if they want to add, subtract, multiple, or divide them.
+# Return a string that prints the two numbers, which operation it did, and the result.
 
 def problem4():
     userInput=int(input("Enter a number."))
